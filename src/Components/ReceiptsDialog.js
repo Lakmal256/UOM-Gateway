01483f875b2a58ecfce20da07f6ae8eeed99b610
Lakmal256 <lakmalfriends@gmail.com>
2022-12-16 23:19:11 +0530

    addviewreceipts

--- src/Components/ReceiptsDialog.js
+++ src/Components/ReceiptsDialog.js
@@ -1,27 +1,79 @@
 import Dialog from "@mui/material/Dialog";
 import "../Dashboard.css";
 import Transfer from "../images/transfer.png";
+import { VIEW } from "../Constants/ViewReceipts";
 import React from "react";
 
 const ReceiptDialog = ({ open, handleClose, openSignup }) => {
   return (
     <div>
       <Dialog
-        classes={{ paper: "submit-form" }}
+        classes={{ paper: "receipts_dialog" }}
         open={open}
         onClose={handleClose}
       >
-        <img className="dialog-title" src={Transfer} alt="firespot" />
-        <div className="title-heading">
-          <div className="sign-in">Sign In</div>
-          <div className="uom-user">UOM User</div>
+        <div className="receipts_image_main">
+          <img className="receipts_image" src={Transfer} alt="firespot" />
         </div>
-        <div className="print_close">
-          <button className="signin">Print</button>
-          <div>
-            <button className="signin" onClick={openSignup}>
-              Close
-            </button>
+        <div className="title-heading">
+          <div className="sign-in">Payment Successfull !!!</div>
+        </div>
+        <div className="receipts_dialog_main">
+          <div className="receipts_data_main">
+            {VIEW.map((field) => {
+              if (field.type === "select") {
+                return (
+                  <div className="form-row" key={field.id}>
+                    <div className="form-label">{field.label}</div>
+                    <select
+                      className={field.className}
+                      disabled={field.disabled}
+                    >
+                      {field.options.map((option) => {
+                        return (
+                          <option key={option.id} value={option.id}>
+                            {option.label}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
+                );
+              } else {
+                return (
+                  <div className="data_row_flex" key={field.left.id}>
+                    <div className="form_row-left">
+                      <div className="form-label-left">{field.left.label}</div>
+                      <div
+                        name={field.left.name}
+                        className={field.left.className}
+                      ></div>
+                    </div>
+                    <div className="form_row-right">
+                      <div className="form-label-right">
+                        {field.right.label}
+                      </div>
+                      <div
+                        name={field.right.name}
+                        className={field.right.className}
+                      ></div>
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="amount_paid_main">
+            <div className="amount_paid">Amount Paid</div>
+            <div className="how_much">LKR 450.00</div>
+          </div>
+          <div className="print_close">
+            <button className="print_button">Print</button>
+            <div>
+              <button className="print_button" onClick={openSignup}>
+                Close
+              </button>
+            </div>
           </div>
         </div>
       </Dialog>
---
+++ src/Constants/ViewReceipts.js
@@ -0,0 +1,84 @@
+export const VIEW = [
+    {
+      left: {
+        id: "transactionId",
+        name: "transactionId",
+        label: "Transaction Id",
+        className: "receipts_data",
+        type: "text",
+        disabled: false,
+        show: true,
+      },
+      right: {
+        id: "#145685345",
+        name: "#145685345",
+        label: "#145685345",
+        className: "receipts_data",
+        type: "text",
+        disabled: false,
+        show: true,
+      },
+    },
+    {
+      left: {
+        id: "paymentCategory",
+        name: "paymentCategory",
+        label: "Payment Category",
+        className: "receipts_data",
+        type: "text",
+        disabled: false,
+        show: true,
+      },
+      right: {
+        id: "libraryFine",
+        name: "libraryFine",
+        label: "Library Fine",
+        className: "receipts_data",
+        type: "text",
+        disabled: false,
+        show: true,
+      },
+    },
+    {
+      left: {
+        id: "paymentDescription",
+        name: "paymentDescription",
+        label: "Payment Description",
+        className: "receipts_data",
+        type: "text",
+        disabled: false,
+        show: true,
+      },
+  
+      right: {
+        id: "256718X_B25_LBR",
+        name: "256718X_B25_LBR",
+        label: "256718X_B25_LBR",
+        className: "receipts_data",
+        type: "text",
+        disabled: false,
+        show: true,
+      },
+    },
+    {
+      left: {
+        id: "email",
+        name: "email",
+        label: "Email",
+        className: "receipts_data",
+        type: "text",
+        disabled: false,
+        show: true,
+      },
+      right: {
+        id: "wijerathna.25@uom.lk",
+        name: "wijerathna.25@uom.lk",
+        label: "wijerathna.25@uom.lk",
+        className: "receipts_data",
+        type: "email",
+        disabled: false,
+        show: true,
+      },
+    },
+  ];
+  
