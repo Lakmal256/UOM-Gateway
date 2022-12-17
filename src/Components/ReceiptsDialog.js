3da29435b942b565f521d9a180624c6ebdfb3397
Lakmal256 <lakmalfriends@gmail.com>
2022-12-17 21:33:59 +0530

    add

--- src/Components/ReceiptsDialog.js
+++ src/Components/ReceiptsDialog.js
@@ -2,9 +2,10 @@ import Dialog from "@mui/material/Dialog";
 import "../Dashboard.css";
 import Transfer from "../images/transfer.png";
 import { VIEW } from "../Constants/ViewReceipts";
+import { ROW } from "../Constants/ReceiptsTableForm";
 import React from "react";
 
-const ReceiptDialog = ({ open, handleClose, openSignup }) => {
+const ReceiptDialog = ({ open, handleClose }) => {
   return (
     <div>
       <Dialog
@@ -20,52 +21,38 @@ const ReceiptDialog = ({ open, handleClose, openSignup }) => {
         </div>
         <div className="receipts_dialog_main">
           <div className="receipts_data_main">
-            {VIEW.map((field) => {
-              if (field.type === "select") {
-                return (
-                  <div className="form-row" key={field.id}>
-                    <div className="form-label">{field.label}</div>
-                    <select
-                      className={field.className}
-                      disabled={field.disabled}
-                    >
-                      {field.options.map((option) => {
-                        return (
-                          <option key={option.id} value={option.id}>
-                            {option.label}
-                          </option>
-                        );
-                      })}
-                    </select>
-                  </div>
-                );
-              } else {
+            {VIEW.map(
+              (field) => {
                 return (
                   <div className="data_row_flex" key={field.left.id}>
                     <div className="form_row-left">
                       <div className="form-label-left">{field.left.label}</div>
-                      <div
-                        name={field.left.name}
-                        className={field.left.className}
-                      ></div>
                     </div>
                     <div className="form_row-right">
                       <div className="form-label-right">
                         {field.right.label}
                       </div>
-                      <div
-                        name={field.right.name}
-                        className={field.right.className}
-                      ></div>
                     </div>
                   </div>
                 );
               }
-            })}
+              // }
+            )}
           </div>
           <div className="amount_paid_main">
             <div className="amount_paid">Amount Paid</div>
-            <div className="how_much">LKR 450.00</div>
+            <div>
+              {ROW.map((field) => {
+                return (
+                  <div className="data_row_flex" key={field.id}>
+                    <div className="form_row-left">
+                      <div className="form-label-left">{field.amount}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            {/* <div className="how_much">LKR 450.00</div> */}
           </div>
           <div className="print_close">
             <button className="print_button">Print</button>
